a3183c43749145a605fc02d7ef1207c3085c94ac
Rowland Uche <rowlanduche@gmail.com>
2023-05-21 15:35:49 +0100

    add ile

--- Pro5/file1.py
+++ Pro5/file1.py
@@ -1,1 +1,14 @@
-# class in python
+#class Vehicle:
+color="white"
+def  _init_(self,max_speed=20,mileage=50000):
+          self.max_speed = max_speed
+          self.mileage = mileage
+    
+def seating_capacity(self,seating_capacity=5):
+          self.seating_capacity=seating_capacity
+def display_properties(self):
+        print("properties of the vehicle:")
+        print("max_speed:",self.max_speed) 
+        print("mileage:",self.mileage)
+        print("color:",self.color)
+        print("seating_capacity:", self.seating_capacity)         
---
+++ Pro5/file2.py
@@ -0,0 +1,4 @@
+import pandas as pd
+filepath='C:\Users\Admin\Desktop\new'
+df=pd.read_excel('filepath','w')
+print(df.read())
